739a7e25bdcd5835c4074faf543cb040b189be98
filipwretner <filip.wretner@chasacademy.se>
2024-10-18 09:57:28 +0200

    Kommentarer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Prompts för att ta emot inmatning
+// Prompts för att ta emot inmatning av nummer
 let firstNumber = parseFloat(prompt("Ange det första talet:"));
 let secondNumber = parseFloat(prompt("Ange det andra talet:"));
 
@@ -10,38 +10,68 @@ if (isNaN(firstNumber) || isNaN(secondNumber)) {
     alert("Felaktig inmatning, testa skriv in ett nummer istället!");
     console.log("Felaktig inmatning");
 } else {
-     operation = prompt("Vill du använda +, -, *, /, % eller **");
 
+   // Prompts för att ta emot inmatning av operation
+   operation = prompt("Vill du använda +, -, *, /, % eller **");
+
+   // if-sats för att genomföra beräkningar
+
+   // Addition
      if (operation === "+") {
+
         result = (firstNumber + secondNumber);
         alert(`Summan är: ${result}`);
         console.log(result);
+
+   // Subraktion
      } else if (operation === "-") {
+
         result = (firstNumber - secondNumber);
         alert(`Differensen är: ${result}`);
         console.log(result);
+
+   // Multiplikation   
      } else if (operation === "*") {
+
         result = (firstNumber * secondNumber);
         alert(`Produkten är: ${result}`);
         console.log(result);
+
+   // Division
      } else if (operation === "/") {
+
+      // Kollar så att vi inte delar med 0
         if (secondNumber !== 0) {
+
             result = (firstNumber / secondNumber);
             alert(`Kvoten är: ${result}`);
             console.log(result);
+
         } else {
+
+         // Felmeddelande om vi delar med 0
             alert("Kan inte dela med 0.");
             console.log("Kan inte dela med 0");
+
         }
+     
+   // Modulus   
      } else if (operation === "%") {
+
         result = (firstNumber % secondNumber);
         alert(`Resterande är: ${result}`);
         console.log(result);
+
+   // Exponentiering 
      } else if (operation === "**") {
+
         result = (firstNumber ** secondNumber);
         alert(`Exponenten är: ${result}`);
         console.log(result);
+
      } else {
+
+      // Felmeddelande om användare skriver in fel vi val av operation
         alert("Ogiltig operation, använd +, -, *, /, % eller **");
         console.log("Ogiltig operation");
      }
